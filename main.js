631dbdf2cce4e2e76572f471762a9748cb231cf9
Tobias Wenck <37943025+tobiaswenck@users.noreply.github.com>
2023-07-14 16:30:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var index = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("brand-icon");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  index++;
+  if (index > x.length) {index = 1}    
+  x[index-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
