82fb10d0b1462fb71b4461e2208f9834568ff6ef
Tobias Wenck <37943025+tobiaswenck@users.noreply.github.com>
2023-07-14 18:02:09 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ var index = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("brand-icon");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  if(window.innerWidth <= 1400) {
+    var i;
+    var x = document.getElementsByClassName("brand-icon");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    index++;
+    if (index > x.length) {index = 1}    
+    x[index-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
   }
-  index++;
-  if (index > x.length) {index = 1}    
-  x[index-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  
 }
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
